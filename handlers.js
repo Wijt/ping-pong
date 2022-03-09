669e3ca7cbaaedadd159c52063801329c999bb14
Furkan Kaya <hello@iamfurkan.com>
2022-03-09 17:19:23 +0300

    multiplayer and admin ui added

---
+++ handlers.js
@@ -0,0 +1,257 @@
+class Player {
+    constructor(id, height, width, x, y, speed, roomsize) {
+        this.id = id;
+        this.height = height;
+        this.width = width;
+
+        this.pos = { x: x, y: y };
+        this.speed = speed;
+
+        this.score = 0;
+
+        this.roomsize = roomsize;
+    }
+
+    up() {
+        var playerTop = this.pos.y;
+        var topY = 0;
+
+        if (playerTop > topY) {
+            this.pos.y -= this.speed;
+        }
+    }
+
+    down() {
+        var playerBottom = this.pos.y + this.height;
+        var bottomY = this.roomsize.h;
+
+        if (playerBottom < bottomY) {
+            this.pos.y += this.speed;
+        }
+    }
+}
+
+class Ball {
+    constructor(startX, startY, radius, speed, color) {
+        this.radius = radius;
+        this.color = color;
+        this.pos = { x: startX, y: startY };
+        this.speed = speed;
+        this.vel = { x: speed, y: 0 };
+
+        this.waitFor = null;
+    }
+
+    update(collisionList) {
+        /*if(this.waitFor) {
+            if(keyIsDown(this.waitFor.upKey)||keyIsDown(this.waitFor.downKey)) {
+                this.waitFor = null;
+            }
+            return;
+        }*/
+        this.collide(collisionList);
+        this.pos = { x: this.pos.x + this.vel.x, y: this.pos.y + this.vel.y };
+    }
+
+    collide(others) {
+        for (let i = 0; i < others.length; i++) {
+            let o = others[i];
+            if (
+                circRect(
+                    this.pos.x,
+                    this.pos.y,
+                    this.radius,
+                    o.pos.x,
+                    o.pos.y,
+                    o.width,
+                    o.height
+                )
+            ) {
+                o.hitEffect(this);
+            }
+        }
+    }
+}
+
+function circRect(cx, cy, rad, rx, ry, rw, rh) {
+    let testX = cx;
+    let testY = cy;
+
+    if (cx < rx) testX = rx;
+    // test left edge
+    else if (cx > rx + rw) testX = rx + rw; // right edge
+    if (cy < ry) testY = ry;
+    // top edge
+    else if (cy > ry + rh) testY = ry + rh; // bottom edge
+
+    let d = Math.sqrt(
+        Math.pow(Math.abs(testX - cx), 2) + Math.pow(Math.abs(testY - cy), 2)
+    );
+    //circle(testX,testY,10)
+    if (d < rad) {
+        return true;
+    }
+    return false;
+}
+
+const map = (value, x1, y1, x2, y2) =>
+    ((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
+
+class Room {
+    constructor(id) {
+        this.id = id;
+        this.players = {};
+        this.size = { w: 1000, h: 700 };
+
+        this.ball = new Ball(
+            this.size.w / 2,
+            this.size.h / 2,
+            20,
+            10,
+            "#B3F2F2"
+        );
+
+        this.gameState = "not started";
+        this.gamePaused = false;
+
+        this.collisionList = [];
+    }
+
+    //add player if there is a room for it
+    addPlayer(player) {
+        if (Object.keys(this.players).length == 1) {
+            //second player must be on the right side
+            player.pos.x = player.roomsize.w - player.pos.x;
+        }
+        if (Object.keys(this.players).length < 2) {
+            this.players[player.id] = player;
+            return true;
+        }
+        return false;
+    }
+
+    start() {
+        this.collisionList = [
+            {
+                pos: Object.values(this.players)[0].pos,
+                width: Object.values(this.players)[0].width,
+                height: Object.values(this.players)[0].height,
+                hitEffect: (ball) => {
+                    let diff =
+                        ball.pos.y - Object.values(this.players)[0].pos.y;
+                    let rad = 45 * (Math.PI / 180);
+                    let angle = map(
+                        diff,
+                        0,
+                        Object.values(this.players)[0].height,
+                        -rad,
+                        rad
+                    );
+                    ball.vel = {
+                        x: ball.speed * Math.cos(angle),
+                        y: ball.speed * Math.sin(angle),
+                    };
+                    ball.pos.x =
+                        Object.values(this.players)[0].pos.x +
+                        Object.values(this.players)[0].width +
+                        ball.radius;
+                },
+            },
+            {
+                pos: Object.values(this.players)[1].pos,
+                width: Object.values(this.players)[1].width,
+                height: Object.values(this.players)[1].height,
+                hitEffect: (ball) => {
+                    let diff =
+                        ball.pos.y - Object.values(this.players)[1].pos.y;
+                    let angle = map(
+                        diff,
+                        0,
+                        Object.values(this.players)[1].height,
+                        225 * (Math.PI / 180),
+                        135 * (Math.PI / 180)
+                    );
+                    ball.vel = {
+                        x: ball.speed * Math.cos(angle),
+                        y: ball.speed * Math.sin(angle),
+                    };
+                    ball.pos.x =
+                        Object.values(this.players)[1].pos.x - ball.radius;
+                },
+            },
+            {
+                pos: { x: 0, y: 0 },
+                width: 10,
+                height: this.size.h,
+                hitEffect: (ball) => {
+                    Object.values(this.players)[1].score += 1;
+                    this.shootBall(-1);
+                    this.restart();
+                },
+            },
+            {
+                pos: { x: 0, y: 0 },
+                width: this.size.w,
+                height: 10,
+                hitEffect: (ball) => {
+                    ball.vel.y *= -1;
+                },
+            },
+            {
+                pos: { x: this.size.w, y: 0 },
+                width: -10,
+                height: this.size.h,
+                hitEffect: (ball) => {
+                    Object.values(this.players)[0].score += 1;
+                    this.shootBall(1);
+                    this.restart();
+                },
+            },
+            {
+                pos: { x: 0, y: this.size.h },
+                width: this.size.w,
+                height: -10,
+                hitEffect: (ball) => {
+                    ball.vel.y *= -1;
+                },
+            },
+        ];
+        this.shootBall(Math.random() < 0.5 ? -1 : 1);
+    }
+
+    update() {
+        if (this.gameState == "not started") {
+            if (Object.keys(this.players).length == 2) {
+                this.gameState = "starting";
+                this.start();
+            }
+            return;
+        }
+
+        if (this.gamePaused) return;
+        this.ball.update(this.collisionList);
+    }
+
+    shootBall(direction) {
+        /*this.ball.waitFor =
+            direction == -1
+                ? Object.values(this.players)[0]
+                : Object.values(this.players)[1];
+        */
+        this.ball.pos = {
+            x: this.size.w / 2 + direction * 300,
+            y: this.size.h / 2,
+        };
+        this.ball.vel = { x: this.ball.speed * direction, y: 0 };
+    }
+
+    restart() {
+        Object.values(this.players)[0].pos.x = this.size.w * 0.1;
+        Object.values(this.players)[0].pos.y = this.size.h / 2 - 250 / 2;
+
+        Object.values(this.players)[1].pos.x = this.size.w - this.size.w * 0.1;
+        Object.values(this.players)[1].pos.y = this.size.h / 2 - 250 / 2;
+    }
+}
+
+module.exports = { Room, Player };
--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,33 @@
+var socket;
+
+var debug = false;
+
+function preload() {
+    socket = io();
+
+    //get roomid and join it if that is not defined ask id to the server and join it;
+    var roomid = getAllUrlParams().roomid;
+
+    if (roomid == null) {
+        roomid = "";
+        socket.emit("create-room");
+        socket.on("set-id", (id) => {
+            roomid = id;
+            socket.emit("join-room", roomid);
+            window.history.pushState("", "", `?roomid=${roomid}`);
+            console.log("room created " + roomid);
+        });
+    } else {
+        socket.emit("join-room", roomid);
+    }
+
+    socket.on("sync", (room) => {
+        sync(room);
+    });
+}
+
+var sceneSize = { w: 1000, h: 700 };
+
 var playerOne;
 var playerTwo;
 var court;
@@ -5,80 +35,55 @@ var ball;
 
 var gamePaused = false;
 
-collisionList=[];
+collisionList = [];
 
 function setup() {
-    createCanvas(innerWidth, innerHeight);
-    court = new Court(innerHeight*0.6, 5, 0, 0, "#ffffff");
-
-    ball = new Ball(innerWidth/2, innerHeight/2, 20, 20, "#B3F2F2");
-
-    playerOne = new Player(250, 20, (innerWidth*0.10), innerHeight/2-250/2, "#ffffff", 87, 83, 10);
-    playerTwo = new Player(250, 20, innerWidth-(innerWidth*0.10), innerHeight/2-250/2, "#ffffff", 38, 40, 10);
-
-    shootBall(Math.random() < 0.5 ? -1 : 1);
-
-    collisionList.push(
-        {
-            pos: playerOne.pos,
-            width: playerOne.width,
-            height: playerOne.height,
-            hitEffect: (ball)=>{
-                let diff = ball.pos.y - playerOne.pos.y;
-                let rad = radians(45);
-                let angle = map(diff, 0, playerOne.height, -rad, rad);
-                ball.vel = createVector(ball.speed * cos(angle), ball.speed * sin(angle));
-                ball.pos.x = playerOne.pos.x + playerOne.width + ball.radius;
-            }
-        }
-    );
-    collisionList.push(
-        {
-            pos: playerTwo.pos,
-            width: playerTwo.width,
-            height: playerTwo.height,
-            hitEffect: (ball)=>{
-                let diff = ball.pos.y - playerTwo.pos.y;
-                let angle = map(diff, 0, playerTwo.height, radians(225), radians(135));
-                ball.vel = createVector(ball.speed * cos(angle), ball.speed * sin(angle));
-                ball.pos.x = playerTwo.pos.x - ball.radius;
-            }
-        }
-    );
-    collisionList.push(
-        {
-            pos: createVector(0, 0),
-            width: 10,
-            height: innerHeight,
-            hitEffect: (ball)=>{playerTwo.score+=1; shootBall(-1); restart();}
-        }
-    ); //Left Wall;
-    collisionList.push(
-        {
-            pos: createVector(0, 0),
-            width: innerWidth,
-            height: 10,
-            hitEffect: (ball)=>{ball.vel.mult(createVector(1, -1));}
-        }
-    ); //Top Wall;
-    collisionList.push(
-        {
-            pos: createVector(innerWidth, 0),
-            width: -10,
-            height: innerHeight,
-            hitEffect: (ball)=>{playerOne.score+=1; shootBall(1); restart();}
-        }
-    ); //Right Wall;
-    collisionList.push(
-        {
-            pos: createVector(0, innerHeight),
-            width: innerWidth,
-            height: -10,
-            hitEffect: (ball)=>{ball.vel.mult(createVector(1, -1));}
-        }
-    ); //Bottom Wall;
+    gameSetup();
 }
 
+function gameSetup() {
+    createCanvas(sceneSize.w, sceneSize.h);
+    court = new Court(sceneSize.h * 0.6, 5, 0, 0, "#ffffff");
+
+    ball = new Ball(sceneSize.w / 2, sceneSize.h / 2, 20, 20, "#B3F2F2");
+
+    playerOne = new Player(
+        250,
+        20,
+        sceneSize.w * 0.1,
+        sceneSize.h / 2 - 250 / 2,
+        "#ffffff",
+        87,
+        83,
+        10
+    );
+    playerTwo = new Player(
+        250,
+        20,
+        sceneSize.w - sceneSize.w * 0.1,
+        sceneSize.h / 2 - 250 / 2,
+        "#ffffff",
+        38,
+        40,
+        10
+    );
+}
+
+function sync(r) {
+    ball.pos = r.ball.pos;
+    ball.vel = r.ball.vel;
+
+    playerOne.pos = Object.values(r.players)[0].pos;
+    playerOne.score = Object.values(r.players)[0].score;
+
+    if (Object.values(r.players)[1] != null) {
+        playerTwo.pos = Object.values(r.players)[1].pos;
+        playerTwo.score = Object.values(r.players)[1].score;
+    }
+
+    gamePaused = r.gamePaused;
+    collisionList = r.collisionList;
+}
 
 function draw() {
     background(color("#0A1119"));
@@ -89,52 +94,47 @@ function draw() {
 
     fill(color("white"));
     textSize(100);
-    textAlign(CENTER, CENTER)
-    text(playerOne.score, innerWidth/2-200, innerHeight*0.15);
-    text(playerTwo.score, innerWidth/2+200, innerHeight*0.15);
-    
+    textAlign(CENTER, CENTER);
+    text(playerOne.score, sceneSize.w / 2 - 200, sceneSize.h * 0.15);
+    text(playerTwo.score, sceneSize.w / 2 + 200, sceneSize.h * 0.15);
 
-    if(gamePaused){
+    if (gamePaused) {
         push();
             rectMode(CORNER);
             fill(0, 0, 0, 200);
-            rect(0, 0, innerWidth, innerHeight);
+            rect(0, 0, sceneSize.w, sceneSize.h);
             rectMode(CENTER);
             fill(color("white"));
-            rect(innerWidth/2, innerHeight/2, 200, 200, 20);
+            rect(sceneSize.w / 2, sceneSize.h / 2, 200, 200, 20);
             fill(color("black"));
-            rect(innerWidth/2-25, innerHeight/2, 25 , 75);
+            rect(sceneSize.w / 2 - 25, sceneSize.h / 2, 25, 75);
             fill(color("black"));
-            rect(innerWidth/2+25, innerHeight/2, 25, 75);
+            rect(sceneSize.w / 2 + 25, sceneSize.h / 2, 25, 75);
         pop();
     }
 
-    if(gamePaused) return;
-    playerOne.update();
-    playerTwo.update();
-    ball.update(collisionList);
+    if (gamePaused) return;
 
-    /*for (let i = 0; i < collisionList.length; i++) { //Show Collider
-        a = collisionList[i];
-        push();
+    if (keyIsDown(38)) {
+        socket.emit("upkey");
+    }
+    if (keyIsDown(40)) {
+        socket.emit("downkey");
+    }
+
+    if (debug) {
+        for (let i = 0; i < collisionList.length; i++) {
+            //Show Collider
+            a = collisionList[i];
+            push();
             fill(color("red"));
             noStroke();
             rect(a.pos.x, a.pos.y, a.width, a.height);
-        pop();
-    }*/
+            pop();
+        }
+    }
 }
 
-function shootBall(direction){
-    ball.waitFor = direction == -1 ? playerOne : playerTwo;
-    ball.pos = createVector(innerWidth/2 + (direction * 300), innerHeight/2);
-    ball.vel = createVector(ball.speed * direction, 0);
-}
-
-function restart(){
-    playerOne.pos.set(innerWidth*0.10, innerHeight/2-250/2);
-    playerTwo.pos.set(innerWidth-(innerWidth*0.10), innerHeight/2-250/2);
-}
-
-function keyPressed(){
-    if(key == "p") gamePaused = !gamePaused;
+function keyPressed() {
+    if (key == "p") socket.emit("pause");
 }
